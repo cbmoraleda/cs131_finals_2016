8af2698da73a37650600afdaf830139f6c649f4e
Christine Felizardo <chrstnnglc@users.noreply.github.com>
2017-04-03 10:30:28 +0800

    Delete Function2_1.java

--- src/main/java/faneallrich_yao/Function2_1.java
+++
@@ -1,8 +0,0 @@
-// This is the second ODE function file for Sys2ODEsRK2.
-package faneallrich_yao;
-
-public class Function2_1{
-	public double compute (double x, double y, double z){
-		return x + y + z;}
-	public static void main (String[] args) {}
-}
